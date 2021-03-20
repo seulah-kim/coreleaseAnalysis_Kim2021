12fb1c612509ef2997c7af5a5d75cbcd9fd4bc05
Seul Ah Kim <seulah_kim@g.harvard.edu>
2021-03-20 10:09:29 -0400

    Update runSimCorelease.m
    added function description and additional comments

--- functions/runSimCorelease.m
+++ functions/runSimCorelease.m
@@ -1,4 +1,16 @@
 function [iNet_cop,iNet_ind,t]=runSimCorelease(nTrials,match_pr)
+% This function accepts two input parameters and simulates independent and co-packaging co-release of glutamate and GABA.
+% Details of this model is described under Methods section in Kim et al. 2021.
+%
+% INPUTS:
+%   nTrial determines number of trials (observations) per run. 
+%   match_pr is a flag used to determine release probabilities of two models
+%      match_pr = 1, release probabilites are set to 0.5 (default) for both models
+%      match_pr = 0, release probability is set to 0.5 for independent and 0.75 for co-packaging. This is to match the failure rate in both models.
+% OUTPUTS:
+%   iNet_cop is a matrix of simulated net post-synaptic current based on the co-packaging model. Dimension is (nTrials x time).
+%   iNet_ind is a matrix of simulated net post-synaptic current based on the independent model. Dimension is (nTrials x time).
+%   t is a time array that matches the column dimension of iNet_cop and iNet_ind.
 
 % parameters 
 sigma = 0.05;  % noise level - 3% of signal
@@ -58,7 +70,9 @@ end
 % shuffle the order in the IPSC to generate independent case
 IPSC_rand=IPSC_ind(randperm(nTrials),:);
 
+% sort trials based on the minimum amplitude size
 [~,sortind_cop]=sort(min(EPSC_cop,[],2));
 [~,sortind_ind]=sort(min(EPSC_ind,[],2));
-iNet_cop = EPSC_cop(sortind_cop,:)+IPSC_cop(sortind_cop,:)+sigma.*signalSize.*randn(size(EPSC_cop));
+% Add uncorrelated gaussian noise to individual observations
+iNet_cop = EPSC_cop(sortind_cop,:)+IPSC_cop(sortind_cop,:)+sigma.*signalSize.*randn(size(EPSC_cop)); 
 iNet_ind = EPSC_ind(sortind_ind,:)+IPSC_rand(sortind_ind,:)+sigma.*signalSize.*randn(size(EPSC_ind));
