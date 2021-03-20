a126077c1809f4f3b407d4d5572bd2b4944ef405
Seul Ah Kim <seulah_kim@g.harvard.edu>
2021-03-20 09:47:42 -0400

    Update preprocess.m
    added function description

--- functions/preprocess.m
+++ functions/preprocess.m
@@ -1,4 +1,5 @@
 function out = preprocess(in)
+% This function accepts an input matrix (observations x time), pre-processes, and outputs an output matrix that are filtered in 3-steps as described in Methods in Kim et al. 2021
 
 % Low pass filter at 2kHz
 Fs=10000;
