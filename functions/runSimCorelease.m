f7c6fa4dc0ba6181b0cf3466cbbc345adb0880b5
Seul Ah Kim <seulah_kim@g.harvard.edu>
2021-08-12 11:30:11 -0400

    Add files via upload
    Add two different noise options (white noise vs. colored noise)

--- functions/runSimCorelease.m
+++ functions/runSimCorelease.m
@@ -1,4 +1,4 @@
-function [iNet_cop,iNet_ind,t]=runSimCorelease(nTrials,match_pr)
+function [iNet_cop,iNet_ind,t]=runSimCorelease(nTrials,match_pr,pE,pI)
 % This function accepts two input parameters and simulates independent and co-packaging co-release of glutamate and GABA.
 % Details of this model is described under Methods section in Kim et al. 2021.
 %
@@ -13,7 +13,7 @@ function [iNet_cop,iNet_ind,t]=runSimCorelease(nTrials,match_pr)
 %   t is a time array that matches the column dimension of iNet_cop and iNet_ind.
 
 % parameters 
-sigma = 0.05;  % noise level - 3% of signal
+sigma = 0.05;  % noise level - 5% of signal
 simT=500; % how long we stimulate after the NT release
 preT=50; % stim Onset 
 
@@ -21,30 +21,21 @@ preT=50; % stim Onset
 TR=0:simT;
 nPoints=simT+preT+1;
 t = linspace(0,nPoints/10,nPoints);  % timescale, each point is 0.1ms
-ratioKinetics = 3; % number of times slower in IPSC (previous 6.667)
-signalSize= 10; % 20pA for both EPSC and IPSC
+ratioKinetics = 3; % number of times slower in IPSC
+signalSize= 10; % 10pA for both EPSC and IPSC
 
-% simplifed EPSC current
+% simplifed EPSC current (this is equivalent to the normalized version of Eqn. 2) 
 Ie=zeros(1,nPoints);
-Ie(preT+TR) = TR.*(exp(-(TR)/10));  % this is equivalent to the normalized version of eqn.
-m=max(Ie(:));
-Ie=-signalSize./m.*Ie;
-% simplifed IPSC current
+Ie(preT+TR) = TR.*(exp(-(TR)/10));  % perform a unit step function 
+m=max(Ie(:)); % this is tau/e 
+Ie=-signalSize./m.*Ie;  % we scale by the signal size
+% simplifed IPSC current (this is equivalent to the normalized version of Eqn. 2) 
 Ii=zeros(1,nPoints);
-Ii(preT+TR) = TR.*(exp(-TR/(10*ratioKinetics)));
-m=max(Ii(:));
-Ii=signalSize./m.*Ii;
-
-clearvars EPSC IPSC plotObj IPSC_rand plotObj_rand
+Ii(preT+TR) = TR.*(exp(-TR/(10*ratioKinetics)));  % perform a unit step function 
+m=max(Ii(:)); % this is tau/e 
+Ii=signalSize./m.*Ii; % we scale by the signal size
  
 EIscale=1.5; % E to I ratio
-
-pReleaseInd=0.5;    % release probability of independent release model
-pReleaseCop=1-(1-pReleaseInd).^2;   % release probability of co-packaging release model
-
-if match_pr % flag for matching pr for two models
-    pReleaseCop = pReleaseInd;
-end
 
 % initialize
 EPSC_cop = zeros(nTrials,length(Ie));
@@ -52,19 +43,63 @@ IPSC_cop = zeros(nTrials,length(Ii));
 EPSC_ind = zeros(nTrials,length(Ie));
 IPSC_ind = zeros(nTrials,length(Ii));
 
-for n=1:nTrials
-    % co-packing model
-    if rand<=pReleaseCop % did release occur?
-        sigmaVesicle=1+0.3*randn; % noise in vesicle content, centered around 1, 30% change
-        EPSC_cop(n,:)=sigmaVesicle.*EIscale.*Ie;
-        IPSC_cop(n,:)=sigmaVesicle.*Ii;
-    end
-    % independent model
-    if rand<=pReleaseInd % did release occur?
-        sigmaVesicle=1+0.3.*randn; % noise in vesicle content, centered around 1, 30% change
-        EPSC_ind(n,:)=sigmaVesicle.*EIscale.*Ie;
-        IPSC_ind(n,:)=sigmaVesicle.*Ii;
-    end
+% two alternate simulation types, depending on specific parameters that we
+% define
+switch nargin
+    case 2  % if pE and pI are not specified
+        pReleaseInd=0.5;    % release probability of independent release model
+        pReleaseCop=1-(1-pReleaseInd).^2;   % release probability of co-packaging release model
+
+        if match_pr % flag for matching pr for two models
+            pReleaseCop = pReleaseInd;
+        end
+        
+        for n=1:nTrials
+            % co-packing model
+            if rand<=pReleaseCop % did release occur?
+                sigmaVesicle=1+0.3*randn; % noise in vesicle content, centered around 1, 30% change
+                EPSC_cop(n,:)=sigmaVesicle.*EIscale.*Ie;
+                IPSC_cop(n,:)=sigmaVesicle.*Ii;
+            end
+            % independent model
+            if rand<=pReleaseInd % did release occur?
+                sigmaVesicle=1+0.3.*randn; % noise in vesicle content, centered around 1, 30% change
+                EPSC_ind(n,:)=sigmaVesicle.*EIscale.*Ie;
+                IPSC_ind(n,:)=sigmaVesicle.*Ii;
+            end
+        end
+    otherwise % if we specify pE and pI
+        % assume same for Glu and GABA so that I can run one simulation and compare
+        % for co-packaging versus independent release using the same trials
+        pReleaseInd_E=pE;
+        pReleaseInd_I=pI;
+        pReleaseCop=sqrt(pE.*pI);   % to match the p(E) and p(I)
+        
+        for n=1:nTrials
+                % use two dies
+                die1 = rand;
+                die2 = rand;
+              % co-packaging model
+              if die1<=pReleaseCop % did release occur?
+                  sigmaVesicle=1+0.3*randn; % noise in vesicle content, centered around 1, 30% change
+                 
+                  EPSC_cop(n,:)=sigmaVesicle.*EIscale.*Ie;
+                  IPSC_cop(n,:)=sigmaVesicle.*Ii;
+              end
+
+              % independent model - two separate dies are used
+              if die1<=pReleaseInd_E
+                  sigmaVesicle=1+0.3.*randn; % noise in vesicle content, centered around 1, 30% change
+
+                  EPSC_ind(n,:)=sigmaVesicle.*EIscale.*Ie;
+              end
+
+              if die2<=pReleaseInd_I
+                  sigmaVesicle=1+0.3.*randn; % noise in vesicle content, centered around 1, 30% change
+                  % independent model
+                  IPSC_ind(n,:)=sigmaVesicle.*Ii;
+              end
+         end 
 end
 
 % shuffle the order in the IPSC to generate independent case
@@ -73,6 +108,22 @@ IPSC_rand=IPSC_ind(randperm(nTrials),:);
 % sort trials based on the minimum amplitude size
 [~,sortind_cop]=sort(min(EPSC_cop,[],2));
 [~,sortind_ind]=sort(min(EPSC_ind,[],2));
-% Add uncorrelated gaussian noise to individual observations
-iNet_cop = EPSC_cop(sortind_cop,:)+IPSC_cop(sortind_cop,:)+sigma.*signalSize.*randn(size(EPSC_cop)); 
-iNet_ind = EPSC_ind(sortind_ind,:)+IPSC_rand(sortind_ind,:)+sigma.*signalSize.*randn(size(EPSC_ind));
+
+% % Add uncorrelated gaussian noise to individual observations
+% iNet_cop = EPSC_cop(sortind_cop,:)+IPSC_cop(sortind_cop,:)+sigma.*signalSize.*randn(size(EPSC_cop));
+% iNet_ind = EPSC_ind(sortind_ind,:)+IPSC_rand(sortind_ind,:)+sigma.*signalSize.*randn(size(EPSC_ind));
+
+% Add 3kHz corner frequency
+fc = 3000;  % cutoff freq
+fs = 10000;  % sampling rate
+% [b,a] = butter(6,fc/(fs/2));    % 6th order lowpass Butterworth filter
+[b,a] = besself(4,fc/(fs/2));   % 4th order (pole) lowpass Bessel filter
+noiseIn_cop=randn(size(EPSC_cop));
+noiseIn_ind=randn(size(EPSC_ind));
+noiseOut_cop=filter(b,a,noiseIn_cop);
+noiseOut_ind=filter(b,a,noiseIn_ind);
+iNet_cop = EPSC_cop(sortind_cop,:)+IPSC_cop(sortind_cop,:)+sigma.*signalSize.*noiseOut_cop;
+iNet_ind = EPSC_ind(sortind_ind,:)+IPSC_rand(sortind_ind,:)+sigma.*signalSize.*noiseOut_ind;
+
+
+
