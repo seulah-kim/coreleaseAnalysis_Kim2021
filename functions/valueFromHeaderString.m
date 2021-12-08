2d493795049040e02703b678f55b77db53c097c7
Seul Ah Kim <seulah_kim@g.harvard.edu>
2021-12-08 13:13:02 -0500

    Add files via upload

---
+++ functions/valueFromHeaderString.m
@@ -0,0 +1,30 @@
+function out=valueFromHeaderString(globalName, header)
+	out='';
+	
+	if nargin<2
+		header=state.headerString;
+	end
+	
+	global state
+	pos=findstr(header, [globalName '=']);
+	
+	if length(pos)==0
+		out=[];
+		return;
+	else
+		cr=findstr(header, 13);
+		index=find(cr>pos);
+		next=cr(index(1));
+		if length(next)==0
+			out=header(pos+length(globalName)+1:end-1);
+		else
+			out=header(pos+length(globalName)+1:next-1);
+		end
+	end
+
+		val=str2num(out);
+		if length(val)==1
+			out=val;
+		else
+			out=out(2:end-1);
+		end
